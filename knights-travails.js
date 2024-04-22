8fe886885529f379e533495ac3fb0a8c8fec384a
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-04-22 18:47:50 +0800

    Create makeBoard function

--- knights-travails.js
+++ knights-travails.js
@@ -6,3 +6,13 @@ const node = (value = null, neighbors = [], distance = null, pred = null) => {
         pred
     };
 };
+
+const makeBoard = (n) => {
+    let board = [];
+
+    for (let x = 0; x < n; x++) {
+        for (let y = 0; y < n; y++) {
+            board[x][y] = [x, y];
+        };
+    };
+};
