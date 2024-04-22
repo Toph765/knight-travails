9fbc10865dfd114eebeccdcb0bf8a0fd3d5638d3
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2024-04-22 19:44:56 +0800

    Create adjacencyList function

--- knights-travails.js
+++ knights-travails.js
@@ -16,3 +16,31 @@ const makeBoard = (n) => {
         };
     };
 };
+
+const adjacencyList = (board) => {
+    let list = [];
+    let moves = [
+        [2, 1], [2, -1],
+        [-2, 1], [-2, -1],
+        [1, 2], [1, -2],
+        [-1, 2], [-1, -2]
+    ];
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board.length; j++) {
+            let x = board[i][j][0];
+            let y = board[i][j][1];
+            let sourceNode = node(board[i][j]);
+
+            moves.forEach(square => {
+                let pair = [x + square[0], y + square[1]];
+                if (pair[0] < 8 && pair[0] >= 0 && pair[1] < 8 && pair[1] >= 0) {
+                    sourceNode.neighbors.push(pair);
+                };
+            });
+
+            list.push(sourceNode);
+        };
+    };
+    return list;
+}
